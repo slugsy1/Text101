300830d8ba0f78679b36f5b501b50e6467ef0128
DESKTOP-VDVB68J\User <chrisjhollings1@outlook.com>
2021-07-28 19:41:06 +0100

    text content

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+   [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
