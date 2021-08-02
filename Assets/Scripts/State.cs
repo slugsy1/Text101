72f59808e5f1bb6a1d99c336f8372a16328d04a9
DESKTOP-VDVB68J\User <chrisjhollings1@outlook.com>
2021-08-02 19:04:39 +0100

    moving

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="State")]
 public class State : ScriptableObject
 {
-  [TextArea(10,14)]  [SerializeField] string stateText;
-
+    [TextArea(10,14)] [SerializeField] string stateText;
+    [SerializeField] State[] nextState;
     public string GetStateStory()
     {
         return stateText;
-
+    }
+    public State[]GetNextState()
+    {
+        return nextState;
     }
 }
